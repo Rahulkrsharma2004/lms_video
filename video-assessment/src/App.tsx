30f2c18e030cbaa06cfeb53e8a94bcf52814871d
Rahulkrsharma2004 <kumarrahulbasdiha@gmail.com>
2024-12-15 22:45:58 +0530

    done

--- video-assessment/src/App.tsx
+++ video-assessment/src/App.tsx
@@ -1,18 +1,10 @@
-import { Outlet } from "react-router-dom"
-import { ThemeProvider } from "./components/theme-provider"
-// import LMSPage from "./components/LMSPage"
-
+import LMSPage from './components/LMSPage'
 
 function App() {
-
   return (
-    <ThemeProvider  defaultTheme="dark" storageKey="vite-ui-theme">
-    <main>
-      <Outlet/>
-    </main>
-      {/* <LMSPage/> */}
-    </ThemeProvider>
-
+    <div>
+      <LMSPage />
+    </div>
   )
 }
 
--- video-assessment/src/main.tsx
+++ video-assessment/src/main.tsx
@@ -1,12 +1,10 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { RouterProvider } from 'react-router-dom'
-import router from './routes/index.ts'
-import { Provider } from 'react-redux'
-import store from './store/store.ts'
+import { StrictMode } from 'react'
+import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <Provider store={store}>
-    <RouterProvider router={router}/>
-  </Provider>,
+  <StrictMode>
+    <App/>
+  </StrictMode>
 )
